41aab0a8359e7559bf592193d4c18741eaf0de77
Sergey Yefanov <sergey.yefanov@mail.ru>
2015-08-15 10:38:23 +0300

    Initial Commit

---
+++ RT_App_Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RT_App_Test
+//
+//  Created by Sergey Yefanov on 15.08.15.
+//  Copyright (c) 2015 Sergey Yefanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RT_App_Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RT_App_Test
+//
+//  Created by Sergey Yefanov on 15.08.15.
+//  Copyright (c) 2015 Sergey Yefanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
